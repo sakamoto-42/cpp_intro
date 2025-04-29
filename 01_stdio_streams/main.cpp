2253e839ae51d78909018fe79328daac6343a064
sakamoto-42 <julien66140@hotmail.fr>
2025-04-29 22:07:11 +0200

    continue with sample class

---
+++ 01_stdio_streams/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julien <julien@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/29 21:24:25 by julien            #+#    #+#             */
+/*   Updated: 2025/04/29 22:01:28 by julien           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(void)
+{
+	char	buffer[512];
+
+	std::cout << "Hello world !" << std::endl;
+	std::cout << "Input a word: ";
+	std::cin >> buffer;
+	std::cout << "You entered: [" << buffer << "]" << std::endl;
+	return (0);
+}
---
+++ 02_class_and_instance/Sample.class.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Sample.class.cpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julien <julien@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/29 21:52:51 by julien            #+#    #+#             */
+/*   Updated: 2025/04/29 22:01:42 by julien           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Sample.class.hpp"
+
+Sample::Sample(void)
+{
+	std::cout << "Constructor called" << std::endl;
+	return ;
+}
+
+Sample::~Sample(void)
+{
+	std::cout << "Destructor called" << std::endl;
+	return ;
+}
