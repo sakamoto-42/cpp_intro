68180095e5fcefee0c5a5326fb161b02a6157e48
sakamoto-42 <julien66140@hotmail.fr>
2025-05-17 17:07:14 +0200

    norm

--- D02/02_canonical_form/Sample.class.hpp
+++ D02/02_canonical_form/Sample.class.hpp
@@ -6,7 +6,7 @@
 /*   By: julien <julien@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 08:54:33 by julien            #+#    #+#             */
-/*   Updated: 2025/05/17 16:04:33 by julien           ###   ########.fr       */
+/*   Updated: 2025/05/17 17:06:39 by julien           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,16 @@
 
 class   Sample  {
     public:
-        Sample(void);							// Canonical (constructeur par defaut)
+		// Canonical (constructeur par defaut)
+        Sample(void);
 		Sample(int const n);
-		Sample(Sample const &src);				// Canonical (constructeur par copie)
-		virtual	~Sample(void);					// Canonical (destructeur)
+		// Canonical (constructeur par copie)
+		Sample(Sample const &src);
+		// Canonical (destructeur)
+		virtual	~Sample(void);
 
-		Sample &operator=(Sample const &rhs);   // Canonical (operateur d'assignation)
+		// Canonical (operateur d'assignation)
+		Sample &operator=(Sample const &rhs);
 
 		int		getFoo(void) const;
 
